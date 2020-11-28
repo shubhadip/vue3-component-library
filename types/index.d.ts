e83ac38634b4e9b0daf0a157b4bc814b0e1aaafd
shubhadip <shubhadip.maity@gmail.com>
2020-11-28 12:33:56 +0530

    refactor: revert changes of index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,8 +3,7 @@ import { DefineComponent, Plugin } from "vue";
 declare const vue3ComponentLibrary: Exclude<Plugin["install"], undefined>;
 export default vue3ComponentLibrary;
 
-export const TestWorld: Exclude<Plugin["install"] & DefineComponent, undefined>;
-export const HelloWorld: Exclude<
-  Plugin["install"] & DefineComponent,
-  undefined
->;
+export const TestWorld: Exclude<Plugin["install"], undefined> | DefineComponent;
+export const HelloWorld:
+  | Exclude<Plugin["install"], undefined>
+  | DefineComponent;
